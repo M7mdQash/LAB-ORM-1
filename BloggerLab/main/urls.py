3c677856b635022e946924ed1fb989998954a794
M7mdQash <sil.qash@protonmail.com>
2026-04-14 22:02:33 +0300

    this lab was easy?, lab done

--- BloggerLab/main/urls.py
+++ BloggerLab/main/urls.py
@@ -3,5 +3,8 @@ from django.urls import path, include
 from . import views
 app_name='main'
 urlpatterns = [
-    path('', views.home_view, name='home_view')
+    path('', views.home_view, name='home_view'),
+    path('blogs/create', views.create_blog_view, name="create_blog_view")
+    # path('blogs/<slug:slug>/', views.blog_details, name='blog_details'),
+
 ]
