4a74592e0bb39e2f4047ee9f4e3bb350af635e85
M7mdQash <sil.qash@protonmail.com>
2026-04-15 15:52:41 +0300

    did deletion and editing

--- BloggerLab/noter/urls.py
+++ BloggerLab/noter/urls.py
@@ -6,5 +6,8 @@ app_name = "noter"
 
 urlpatterns = [
     path('blogs/create', views.create_blog_view, name="create_blog_view"),
-    path('details/<int:blog_id>', views.blog_details_view, name='blog_details_view')
+    path('details/<int:blog_id>', views.blog_details_view, name='blog_details_view'),
+    path('details/<int:blog_id>/update', views.blog_update_view, name='blog_update_view'),
+    path('details/<int:blog_id>/delete', views.blog_delete_view, name='blog_delete_view'),
+
 ]
--- BloggerLab/noter/views.py
+++ BloggerLab/noter/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.http import HttpResponse, HttpRequest
 from django.db import models
 from .models import Blog
@@ -24,3 +24,25 @@ def blog_details_view(request:HttpRequest,blog_id:int):
     blog = Blog.objects.get(pk=blog_id)
     print(blog)
     return render(request,'noter/blog_details.html',{"blog":blog})
+
+def blog_update_view(request:HttpRequest,blog_id:int):
+    blog = Blog.objects.get(pk=blog_id)
+    if request.method == "POST":
+        blog.title = request.POST["title"]
+        blog.content = request.POST["content"]
+        blog.is_published = request.POST.get("is_published") == "on"
+        blog.published_at = request.POST["published_at"]
+        if request.FILES.get("image"):
+            blog.image = request.FILES["image"]
+        if request.FILES.get("file"):
+            blog.files = request.FILES["file"]
+        blog.save()
+        
+        return redirect("noter:blog_details_view",blog_id=blog_id)
+    return render(request,'noter/blog_update.html',{"blog":blog})
+
+
+def blog_delete_view(request:HttpRequest,blog_id:int):
+    blog = Blog.objects.get(pk=blog_id)
+    blog.delete()
+    return redirect("main:home_view")
